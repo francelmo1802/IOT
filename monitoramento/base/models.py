ffde4bc5e28a42077dd1fc2004dbca41194163a7
Francelmo <Francelmo87@gmail.com>
2023-09-18 22:48:49 -0300

    criado os modelos de banco de dados comun ao proje

--- monitoramento/base/models.py
+++ monitoramento/base/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampsModel(models.Model):
+    data = models.DateField(
+        'data',
+        null=True,
+        blank=True,
+        auto_now_add=True,
+        auto_now=False,
+    )
+    hora = models.TimeField(
+        'hora',
+        null=True,
+        blank=True,
+        auto_now_add=True,
+        auto_now=False,
+    )
+
+
+    def __str__(self):
+        return f'{self.data.strftime("%d-%m-%Y")} - {self.hora}'
+
+    class Meta:
+        abstract = True
