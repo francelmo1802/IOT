708b8b2583cfc75bba6af59b2c5fed69ad65c1fd
Francelmo <Francelmo87@gmail.com>
2023-09-19 16:09:18 -0300

    indicando a pasta padrão para os templates

--- monitoramento/settings.py
+++ monitoramento/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'monitoramento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
