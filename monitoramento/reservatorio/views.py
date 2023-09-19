41cb65264c7ee95850af31ad8432fa5b8173801c
Francelmo <Francelmo87@gmail.com>
2023-09-19 20:25:04 -0300

    Criado o acesso apenas para os usuarios  logados

--- monitoramento/reservatorio/views.py
+++ monitoramento/reservatorio/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 
+@login_required
 def reservatorio(request):
     template_name = 'reservatorio.html'
+    # context = {'scopo': aplication_iot.get_data_device()}
     return render(request, template_name)
