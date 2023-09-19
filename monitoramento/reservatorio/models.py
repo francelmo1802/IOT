d643b9251de6152841a5d5125e871eb55300cfb7
Francelmo <Francelmo87@gmail.com>
2023-09-19 15:46:37 -0300

    criado os modelos de fato

--- monitoramento/reservatorio/models.py
+++ monitoramento/reservatorio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from monitoramento.base.models import TimeStampsModel
+
+class Reservatorio(TimeStampsModel):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    active = models.BooleanField('ativo', default=True)
+    name = models.CharField('nome', max_length=30)
+    valor = models.FloatField('valor', null=True, blank=True)
+
+    class Meta:
+        ordering = ('data', 'hora')
+
+    def __str__(self):
+        return f'{self.nome}-{self.valor:.2f}'
