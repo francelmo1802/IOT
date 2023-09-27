2dd98947b86eb98e032c692d8e06b4ef457a8712
Francelmo <Francelmo87@gmail.com>
2023-09-27 16:21:14 -0300

    criado a admin da app reservatorio

--- monitoramento/reservatorio/admin.py
+++ monitoramento/reservatorio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from monitoramento.reservatorio.models import Reservatorio
+
+
 # Register your models here.
+
+
+@admin.register(Reservatorio)
+class ReservatorioAdmin(admin.ModelAdmin):
+    list_display = ('data','hora','name','value',)
