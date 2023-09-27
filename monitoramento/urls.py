68058ea4e5bfa7f590102cd2a17e8347a875685a
Francelmo <Francelmo87@gmail.com>
2023-09-27 16:20:34 -0300

    Ajuste nas rotas

--- monitoramento/urls.py
+++ monitoramento/urls.py
@@ -4,9 +4,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    # path('login/', auth_views.LoginViews.as_view(), name='login'),
-    # path('logout/', auth_views.LoginViews.as_view(), name='login'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+   
     path('reservatorio/', include('monitoramento.reservatorio.urls')),
     path('', include('monitoramento.base.urls')),
     path('admin/', admin.site.urls),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
