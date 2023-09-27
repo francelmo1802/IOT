9887c75be20db620bff2103210f68e6a95fa9889
Francelmo <Francelmo87@gmail.com>
2023-09-27 16:23:01 -0300

    inserido as rotas das API's do liquid e das linhas

--- monitoramento/reservatorio/urls.py
+++ monitoramento/reservatorio/urls.py
@@ -5,5 +5,7 @@ from . import views as v
 app_name = 'monitoramento.reservatorio'
 
 urlpatterns = [
-    path('',v.reservatorio, name = 'reservatorio')
+    path('',v.reservatorio, name = 'reservatorio'),
+    path('liquid/', v.liquid, name = 'liquid'),
+    path('linha/', v.linha, name='linha'),
 ]
