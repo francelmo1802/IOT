893032a58ec4c7d978f681883ae2184b0483378c
Francelmo <Francelmo87@gmail.com>
2023-09-27 16:22:12 -0300

    alteração no nome do campo valor para value

--- monitoramento/reservatorio/models.py
+++ monitoramento/reservatorio/models.py
@@ -7,10 +7,10 @@ class Reservatorio(TimeStampsModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     active = models.BooleanField('ativo', default=True)
     name = models.CharField('nome', max_length=30)
-    valor = models.FloatField('valor', null=True, blank=True)
+    value = models.FloatField('valor', null=True, blank=True)
 
     class Meta:
         ordering = ('data', 'hora')
 
     def __str__(self):
-        return f'{self.nome}-{self.valor:.2f}'
+        return f'{self.value:.2f}'
